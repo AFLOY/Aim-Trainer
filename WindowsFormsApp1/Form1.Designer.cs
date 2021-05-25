8f6deac00fb295c3eb582b6cc5ba1d8130f18f0b
浩之 山田 <H44bcda@gmail.com>
2021-05-25 17:32:17 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.ドルフロ_壁紙;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.ドルフロ_壁紙;
+            this.pictureBox2.Location = new System.Drawing.Point(237, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.ドルフロ_壁紙;
+            this.pictureBox3.Location = new System.Drawing.Point(237, 218);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 521);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Location sets
+        int pl1;
+        int pl2;
+        int pl3;
+        Point pt1 = new Point(12,12);
+        Point pt2 = new Point(12,93);
+        Point pt3 = new Point(12,174);
+        Point pt4 = new Point(12,255);
+        Point pt5 = new Point(12,336);
+        Point pt6 = new Point(12,417);
+        Point pt7 = new Point(93,12);
+        Point pt8 = new Point(93,93);
+        Point pt9 = new Point(93,174);
+        Point pt10 = new Point(93,255);
+        Point pt11 = new Point(93,336);
+        Point pt12 = new Point(93,417);
+        Point pt13 = new Point(174,12);
+        Point pt14 = new Point(174,93);
+        Point pt15 = new Point(174,174);
+        Point pt16 = new Point(174,255);
+        Point pt17 = new Point(174,336);
+        Point pt18 = new Point(174,417);
+        Point pt19 = new Point(255,12);
+        Point pt20 = new Point(255,93);
+        Point pt21 = new Point(255,174);
+        Point pt22 = new Point(255,336);
+        Point pt23 = new Point(255,417);
+        Point pt24 = new Point(336,12);
+        Point pt25 = new Point(336,93);
+        Point pt26 = new Point(336,174);
+        Point pt27 = new Point(336,255);
+        Point pt28 = new Point(336,336);
+        Point pt29 = new Point(336,417);
+        Point pt30 = new Point(417, 12);
+        Point pt31 = new Point(417, 93);
+        Point pt32 = new Point(417, 147);
+        Point pt33 = new Point(417, 255);
+        Point pt34 = new Point(417, 336);
+        Point pt35 = new Point(417, 417);
+
+        //point couter
+        int point;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            point = point + 1000;
+            Random r1 = new Random();
+            int p1 = r1.Next(1, 35);
+            if (p1 == 1)
+            {
+                pictureBox1.Location = pt1;
+            }
+            if (p1 == 2)
+            {
+                pictureBox1.Location = pt2;
+            }
+            if (p1 == 3)
+            {
+                pictureBox1.Location = pt3;
+            }
+            if (p1 == 4)
+            {
+                pictureBox1.Location = pt4;
+            }
+            if (p1 == 5)
+            {
+                pictureBox1.Location = pt5;
+            }
+            if (p1 == 6)
+            {
+                pictureBox1.Location = pt6;
+            }
+            if (p1 == 7)
+            {
+                pictureBox1.Location = pt7;
+            }
+            if (p1 == 8)
+            {
+                pictureBox1.Location = pt8;
+            }
+            if (p1 == 9)
+            {
+                pictureBox1.Location = pt9;
+            }
+            if (p1 == 10)
+            {
+                pictureBox1.Location = pt10;
+            }
+            if (p1 == 11)
+            {
+                pictureBox1.Location = pt11;
+            }
+            if (p1 == 12)
+            {
+                pictureBox1.Location = pt12;
+            }
+            if (p1 == 13)
+            {
+                pictureBox1.Location = pt13;
+            }
+            if (p1 == 14)
+            {
+                pictureBox1.Location = pt14;
+            }
+            if (p1 == 15)
+            {
+                pictureBox1.Location = pt15;
+            }
+            if (p1 == 16)
+            {
+                pictureBox1.Location = pt16;
+            }
+            if (p1 == 17)
+            {
+                pictureBox1.Location = pt17;
+            }
+            if (p1 == 18)
+            {
+                pictureBox1.Location = pt18;
+            }
+            if (p1 == 19)
+            {
+                pictureBox1.Location = pt19;
+            }
+            if (p1 == 20)
+            {
+                pictureBox1.Location = pt20;
+            }
+            if (p1 == 21)
+            {
+                pictureBox1.Location = pt21;
+            }
+            if (p1 == 22)
+            {
+                pictureBox1.Location = pt22;
+            }
+            if (p1 == 23)
+            {
+                pictureBox1.Location = pt23;
+            }
+            if (p1 == 24)
+            {
+                pictureBox1.Location = pt24;
+            }
+            if (p1 == 25)
+            {
+                pictureBox1.Location = pt25;
+            }
+            if (p1 == 26)
+            {
+                pictureBox1.Location = pt26;
+            }
+            if (p1 == 27)
+            {
+                pictureBox1.Location = pt27;
+            }
+            if (p1 == 28)
+            {
+                pictureBox1.Location = pt28;
+            }
+            if (p1 == 29)
+            {
+                pictureBox1.Location = pt29;
+            }
+            if (p1 == 30)
+            {
+                pictureBox1.Location = pt30;
+            }
+            if (p1 == 31)
+            {
+                pictureBox1.Location = pt31;
+            }
+            if (p1 == 32)
+            {
+                pictureBox1.Location = pt32;
+            }
+            if (p1 == 33)
+            {
+                pictureBox1.Location = pt33;
+            }
+            if (p1 == 34)
+            {
+                pictureBox1.Location = pt34;
+            }
+            if (p1 == 35)
+            {
+                pictureBox1.Location = pt35;
+            }
+
+        }
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
